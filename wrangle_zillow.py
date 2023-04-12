e685cc09443ca924ca2c6eff00da9b9594e3d4b3
Madeleine Capper <madeleine@codeup.com>
2023-04-12 16:59:24 -0500

    add notes in acq

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -20,9 +20,12 @@ warnings.filterwarnings('ignore')
 # narrow down to single unit properties (we will do this with pandas so we can investigate the fields)
 query = '''
 SELECT
+-- everything from properties_2017 as prop:
     prop.*,
+-- logerror and transactiondate from predictions_2017:
     predictions_2017.logerror,
     predictions_2017.transactiondate,
+-- all the extra struff for left joins:
     air.airconditioningdesc,
     arch.architecturalstyledesc,
     build.buildingclassdesc,
@@ -30,14 +33,25 @@ SELECT
     landuse.propertylandusedesc,
     story.storydesc,
     construct.typeconstructiondesc
+-- FROM our first table, prperties 2017, as prop
 FROM properties_2017 prop
+-- We want to make sure that we are only grabbing units with a sale in 2017, and
+-- most notably the transactions that happened most recently.
+-- for every property, the maximum (most recent date and the property/parcelid
+-- associated with that transaction.  This will give us a two-column table to
+-- narrow down our prop table using an inner join.
 JOIN (
     SELECT parcelid, MAX(transactiondate) AS max_transactiondate
     FROM predictions_2017
     GROUP BY parcelid
 ) pred USING(parcelid)
+-- now that we have the properties table narrowed down to our specific parameters,
+-- use those specific parameters to join both on parcelid and the max date.
+-- we want to join on the instances that we narrowed down in the subquery. 
+-- think about this as a join on a concatenated set of two columns.
 JOIN predictions_2017 ON pred.parcelid = predictions_2017.parcelid
                       AND pred.max_transactiondate = predictions_2017.transactiondate
+                      -- everything else from here will be a left join, as we have the data set at the size we want and we dont want to make it smaller by reducing it based on missing columns in the following:
 LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
 LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
 LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
@@ -45,8 +59,10 @@ LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
 LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
 LEFT JOIN storytype story USING (storytypeid)
 LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
+-- give me only properties where i have lattitude and longitude values:
 WHERE prop.latitude IS NOT NULL
   AND prop.longitude IS NOT NULL
+-- give me only properties with that max transaction date in 2017
   AND transactiondate <= '2017-12-31'
 '''
 
@@ -92,9 +108,9 @@ def handle_missing_values(df, prop_required_column, prop_required_row):
     drop the missing values per the axis contingent on the amount of data present.
     '''
     n_required_column = round(df.shape[0] * prop_required_column)
+    df = df.dropna(axis=1, thresh=n_required_column)
     n_required_row = round(df.shape[1] * prop_required_row)
     df = df.dropna(axis=0, thresh=n_required_row)
-    df = df.dropna(axis=1, thresh=n_required_column)
     return df
 
 def acquire():
